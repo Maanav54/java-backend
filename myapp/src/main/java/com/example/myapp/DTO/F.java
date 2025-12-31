05f63c9c9b57ce25016094deb658647023ead502
Maanav54 <ponugotimaanavirao@gmail.com>
2025-12-31 15:27:07 +0530

    dto

---
+++ myapp/src/main/java/com/example/myapp/DTO/F.java
@@ -0,0 +1,7 @@
+package com.example.myapp.DTO;
+
+public class F {
+    public Object apple="🍎";
+    public Object peach="🍑";
+    
+}
---
+++ myapp/src/main/java/com/example/myapp/Index.java
@@ -0,0 +1,18 @@
+package com.example.myapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.myapp.DTO.F;
+
+@RestController
+public class Index {
+    @GetMapping("/")
+    F m1() {
+        return new F();
+    }
+    @GetMapping("/a")
+    String m2(){
+        return "Hello";
+    }
+}
